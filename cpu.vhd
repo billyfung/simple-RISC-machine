5e053e22f29f501779d6fbb351e06b05f527907f
Billy Fung <billy@billyfung.com>
2014-10-23 13:52:19 -0700

    alpha version of next state logic, missing datapath signals

--- cpu.vhd
+++ cpu.vhd
@@ -55,25 +55,29 @@ entity cpu is
 end;
 
 architecture impl of controller is
-type state_type is (loadIR, updatePC, decode, readrm, alu, computeaddress, writerdrn);
+type state_type is (loadIR, updatePC, decode, readrm, alu, computeaddress, load, writerdrn);
 signal current_state, next_state, next1: state_type;
 signal Rn, Rd, Rm: std_logic_vector(2 downto 0);
 
 --perhaps it would be easier to use states defined by their opcode
 
-reset<='1';
-loadpc<='0';
-loadir<='0';
-msel<='1';
-vsel<= "11";
+reset <='1';
+loadpc <='0';
+loadir <='0';
+msel <='0';
+vsel <= "11";
+nsel <= "11";
+aluop <= op;
 
 process(all) begin
     case current_state is
       when loadir =>  
-        next1 <= updatepc; -- 
+        --load instruction into IR
+        next1 <= updatepc; 
         msel <= '0';
         reset <= '0';
         loadpc <= '0';
+        loadir <= '1';
 
   	  when updatepc => 
         next1 <= decode; 
@@ -81,9 +85,10 @@ process(all) begin
 
   	  when decode => 
       -- read Rn into register A
+      --decode and determine which opcode state to go into
         read_value <= Rm;
         loada <= 1;
-        nsel<="11";
+        nsel <= "11";
 
         case? opcode&op is
           when "11010" => next1 <= writerdrn; --mov Rn,#<imm8>
@@ -94,24 +99,47 @@ process(all) begin
         end case?;
 
       when readrm =>
-        -- decode and determine which opcode state to go into
-        loadb <= 0;
+        -- load Rm into register B
+        loadb <= '1';
+        loada <= '0';
+        nsel <= "01";
+        bsel <= '0';
         case? opcode&op is
-          when "101--" => next1 <= alu;
-          when "11000" => next1 <= writerdrn; asel <= 1; bsel<=0; loadc<=1;
+          when "101--" => next1 <= alu; -- alu options
+          when "11000" => next1 <= writerdrn; asel <= 1; bsel<=0; loadc<=1; --mov rd
           when others => next1 <= '-';
         end case?;
 
       when alu =>
-          aluop <= op;
-          loadc <= 1;
-          next1<=
+        loadb <= 0;
+        aluop <= op;
+        loadc <= 1;
+        next1 <= writerdrn;
 
-          when others =>  state <= S1;
-          end case;
-      when others =>  
-        next1 <= std_logic_vector'(SWIDTH-1 downto 0 => '-'); 
-        lights <= "------";
+      when computeaddress =>
+        bsel <= '1'; --bin is sximm5
+        aluop <= "00";
+        loadc <= '1' -- load the value of alu into register c
+        case opcode is
+          when "011" => next1 <= load; 
+          when "100" => next1 <= writerdrn;
+          when others => next1 <= loadir;
+        end case;
+
+      when load => 
+        --mdata = output of register C
+        msel <= '1';
+        next1 <= writerdrn;
+
+      when writerdrn =>
+          case? opcode&op is
+            when "11010" => next1 <= loadir; rd <= sximm8; nsel <= "11"; --mov rn
+            when "10000" => next1 <= loadir; rd <= cout; mwrite <= '1'; --str 
+            when "101--" => next1 <= loadir; rd <= cout; nsel <="10"; -- alu operations
+            when "01100" => next1 <= loadir; rd <= mdata; nsel <= 10;
+            when others => next1 <= loadir;
+          end case?
+      when others => next1 <= loadir;
     end case;
   end process;
 
