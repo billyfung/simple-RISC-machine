08105dfc6eac7b8c1c38306601ff9438ea249e45
Billy Fung <billy@billyfung.com>
2014-10-23 21:01:10 -0700

    changed up vhdl coding style to not have nested case statements

--- cpu.vhd
+++ cpu.vhd
@@ -8,13 +8,12 @@ use work.datapath_declarations.all;
 
 package controller_declarations is
 
-  constant MOV: std_logic_vector(2 downto 0) := "110"
-  constant ALU: std_logic_vector(2 downto 0) := "101"
-  constant LDR: std_logic_vector(2 downto 0) := "011"
-  constant STR: std_logic_vector(2 downto 0) := "100"
+  constant MOV: std_logic_vector(2 downto 0) := "110";
+  constant ALU: std_logic_vector(2 downto 0) := "101";
+  constant LDR: std_logic_vector(2 downto 0) := "011";
+  constant STR: std_logic_vector(2 downto 0) := "100";
 
   component controller is
-    generic( );
     port (reset        : in std_logic;
 		  opcode	   : in std_logic_vector(2 downto 0);
 		  op		   : in std_logic_vector(1 downto 0);
@@ -55,95 +54,67 @@ entity cpu is
 end;
 
 architecture impl of controller is
-type state_type is (loadIR, updatePC, decode, readrm, alu, computeaddress, load, writerdrn);
+type state_type is (loadIR, updatePC, decode, readrm, alu, memory, writerdrn);
 signal current_state, next_state, next1: state_type;
 signal Rn, Rd, Rm: std_logic_vector(2 downto 0);
 
 --perhaps it would be easier to use states defined by their opcode
+begin
 
-reset <='1';
-loadpc <='0';
-loadir <='0';
-msel <='0';
-vsel <= "11";
-nsel <= "11";
+current_state <= state_type'val(to_integer(unsigned(state_type)));
+next_state <= state_type'val(to_integer(unsigned(state_type)));
+next1 <= state_type'val(to_integer(unsigned(state_type)));
+
 aluop <= op;
 
 process(all) begin
-    case current_state is
-      when loadir =>  
-        --load instruction into IR
-        next1 <= updatepc; 
-        msel <= '0';
-        reset <= '0';
-        loadpc <= '0';
-        loadir <= '1';
-
-  	  when updatepc => 
-        next1 <= decode; 
-        loadpc <= '1';
-
-  	  when decode => 
-      -- read Rn into register A
-      --decode and determine which opcode state to go into
-        read_value <= Rm;
-        loada <= 1;
-        nsel <= "11";
-
-        case? opcode&op is
-          when "11010" => next1 <= writerdrn; --mov Rn,#<imm8>
-          when "101--" => next1 <= readrm; --read rm state
-          when "01100" => next1 <= computeaddress; --compute address for ldr str
-          when "10000" => next1 <= computeaddress; -- compute address for ldr str
-          when others => next1 <= '-';
-        end case?;
-
-      when readrm =>
-        -- load Rm into register B
-        loadb <= '1';
-        loada <= '0';
-        nsel <= "01";
-        bsel <= '0';
-        case? opcode&op is
-          when "101--" => next1 <= alu; -- alu options
-          when "11000" => next1 <= writerdrn; asel <= 1; bsel<=0; loadc<=1; --mov rd
-          when others => next1 <= '-';
-        end case?;
-
-      when alu =>
-        loadb <= 0;
-        aluop <= op;
-        loadc <= 1;
-        next1 <= writerdrn;
-
-      when computeaddress =>
-        bsel <= '1'; --bin is sximm5
-        aluop <= "00";
-        loadc <= '1' -- load the value of alu into register c
-        case opcode is
-          when "011" => next1 <= load; 
-          when "100" => next1 <= writerdrn;
-          when others => next1 <= loadir;
-        end case;
-
-      when load => 
-        --mdata = output of register C
-        msel <= '1';
-        next1 <= writerdrn;
-
-      when writerdrn =>
-          case? opcode&op is
-            when "11010" => next1 <= loadir; rd <= sximm8; nsel <= "11"; --mov rn
-            when "10000" => next1 <= loadir; rd <= cout; mwrite <= '1'; --str 
-            when "101--" => next1 <= loadir; rd <= cout; nsel <="10"; -- alu operations
-            when "01100" => next1 <= loadir; rd <= mdata; nsel <= 10;
-            when others => next1 <= loadir;
-          end case?
+    case? current_state&opcode&op is
+      when loadir & "-----" => next1 <= updatepc;
+      when updatepc & "-----" => next1 <= decode;
+      when decode & "11010" => next1 <= writerdrn; --mov rn
+      when decode & "10---" => next1 <= readrm; --alu & str
+      when decode & "011---" => next1 <= readrm; --ldr
+      when readrm & "110---" => next1 <= writerdrn; -- mov rd
+      when readrm & "101---" => next1 <= alu; -- add, cmp, and, mvn
+      when readrm & "011---" => next1 <= alu; --ldr
+      when readrm & "100---" => next1 <= alu; --str
+      when alu & "10101" => next1 <= loadir; --cmp
+      when alu & "10100" => next1 <= writerdrn; --add
+      when alu & "10110" => next1 <= writerdrn; --and
+      when alu & "10111" => next1 <= writerdrn; --mvn
+      when alu & "01100" => next1 <= memory; --ldr
+      when alu & "10000" => next1 <= memory; --str
+      when memory & "011--" => next1 <= writerdrn; --ldr to write
+      when memory & "100--" => next1 <= loadir; --str to memory & then next instruction
+      when writerdrn & "-----" => next1 <= loadir;
       when others => next1 <= loadir;
-    end case;
-  end process;
+    end case?
+end process;
+
+loadir <= '1' when current_state = loadir else '0';
+loadpc <= '1' when current_state = updatepc else '0';
+read_value <= Rm when current_state = decode else "000";
+loada <= '1' when current_state = decode else '0'; -- Rm into regA
+loadb <= '1' when current_state = readrm else '0';
+--Rn <= sximm8 when current_state&opcode = writerdrn & "10" else "---"; -- rn=sximm8 
+nsel <= "11" when current_state & opcode = writerdrn & "10" else "10"; -- nsel = rn for mov_rn, else nsel =rd
+loadc <= '1' when current_state = alu else '0';
+--status flag <= cmp stuff
+asel <= '1' when current_state & opcode = writerdrn & "00" else '0;' -- rd = shifted_rm
+
+  case current_state&opcode is
+    when alu&"011" => bsel <= '1';
+    when alu&"100" => bsel <= '1';
+    when others => bsel <= '0';
+  end case;
+msel <= '1' when bsel = '1' else '0';
+mwrite <= '1' when current_state & opcode = memory & "100"
+vsel <= "10" when current_state & opcode&op = writerdrn & "110" & "00" else 
+        "11" when current_state & opcode = writerdrn & "011"
+
+
 
   -- add reset
   next_state <= loadir when reset else next1;
   
- end process
+ end;
